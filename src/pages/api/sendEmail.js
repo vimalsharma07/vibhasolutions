b60283c9a9e2e26ecac348809ea0caa9126d8604
Vimal Sharma <vimalsharma0753@gmail.com>
2024-10-07 17:51:37 +0530

    update mail

--- src/pages/api/sendEmail.js
+++ src/pages/api/sendEmail.js
@@ -5,26 +5,23 @@ export default async function handler(req, res) {
     if (req.method === 'POST') {
       const { name, email, message } = req.body;
 
-      // Create a transporter using your SMTP settings from environment variables
       const transporter = nodemailer.createTransport({
         host: process.env.MAIL_HOST,
         port: Number(process.env.MAIL_PORT),
-        secure: process.env.MAIL_ENCRYPTION === 'ssl', // true for 465, false for other ports
+        secure: process.env.MAIL_ENCRYPTION === 'ssl',
         auth: {
           user: process.env.MAIL_USERNAME,
           pass: process.env.MAIL_PASSWORD,
         },
       });
 
-      // Define the email options
       const mailOptions = {
         from: `${process.env.MAIL_FROM_NAME} <${process.env.MAIL_FROM_ADDRESS}>`,
-        to: 'vimalsharma0753@gmail.com', // Change this to your recipient
+        to: 'vimalsharma0753@gmail.com',
         subject: 'New Contact Form Submission',
         text: `Message from ${name} <${email}>: ${message}`,
       };
 
-      // Send the email
       await transporter.sendMail(mailOptions);
       res.status(200).json({ message: 'Email sent successfully!' });
     } else {
